0d4187ac8e753d181522a390a322d78e964e1ad0
benjaminmay1 <132348762+benjaminmay1@users.noreply.github.com>
2024-11-05 16:44:52 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -20,19 +20,22 @@
             python3Packages.pip
             python3Packages.virtualenv
             nodePackages.cdktf-cli
-            pipenv
             docker
             poetry
-            unzip  
+            unzip
+            azure-cli
           ];
 
           shellHook = ''
             echo "Terraform CDK Python environment with Poetry activated"
             echo "Run 'cdktf --help' to see available commands"
             echo "Run 'poetry --version' to check Poetry installation"
+            echo "Run 'az --version' to check Azure CLI installation"
             echo ""
             echo "To initialize a new CDKTF project with Python and Poetry, run:"
             echo "cdktf init --template=\"https://github.com/johnfraney/cdktf-remote-template-python-poetry/archive/refs/heads/main.zip\" --local"
+            echo ""
+            echo "For WSL users: Remember to set up the Azure credentials symlink as described in the README.md"
           '';
         };
       }
